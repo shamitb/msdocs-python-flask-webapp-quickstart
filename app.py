03bec095783fdb32dee0a0c1267ac8953731a660
Shamit Bagchi <shamit@gmail.com>
2023-05-23 14:26:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,9 @@ def home():
     access_token = request.headers.get('Authorization')
     user_principal = request.headers.get('X-User-Principal')
 
-    if access_token:
-        # Use the access token for further validation or authorization
-        print(f"Access token: {access_token}")
-
-    if user_principal:
-        # Use the user principal value in your logic
-        print(f"User principal: {user_principal}")
+    #if access_token:
+    # Use the access token for further validation or authorization
+    print(dict(request.headers))
 
     return "Request received"
 
