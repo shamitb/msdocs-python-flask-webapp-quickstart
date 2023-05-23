cf3aa839ec13980d4aa62b05067e866fb4265d77
Shamit Bagchi <shamit@gmail.com>
2023-05-23 15:00:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     access_token = request.headers.get('Authorization')
-    user_principal = request.headers.get('X-User-Principal')
+    user_principal = request.headers.get('X-Ms-Client-Principal-Name')
 
     #if access_token:
     # Use the access token for further validation or authorization
     print(dict(request.headers))
 
-    return "Request received"
+    return user_principal
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
