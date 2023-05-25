abe81adb7a0e56d415d3ef8366ea2e6750b3c54b
Shamit Bagchi <shamit@gmail.com>
2023-05-25 11:59:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
 from flask import Flask, request
 
-app = Flask(__name__)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-@app.route('/')
-def home():
-    access_token = request.headers.get('Authorization')
-    user_principal = request.headers.get('X-Ms-Client-Principal-Name')
+app = Dash(__name__)
+user_principal = request.headers.get('X-Ms-Client-Principal-Name')
 
-    #if access_token:
-    # Use the access token for further validation or authorization
-    print(dict(request.headers))
+app.layout = html.Div([
+    html.H1(children=user_principal, style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
 
-    return user_principal
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run_server(debug=True)
