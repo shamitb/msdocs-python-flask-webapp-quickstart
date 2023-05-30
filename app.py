875b26850a8a53d6e9f48c51a8579e9f74019385
Shamit Bagchi <shamit@gmail.com>
2023-05-30 17:12:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
 import dash
-from dash.dependencies import Input, Output
-import flask
+import dash_core_components as dcc
+import dash_html_components as html
 
-app = dash.Dash(__name__)
+dash_app = dash.Dash()
+app = dash_app.server
 
-# Define a callback function that retrieves the client principal name
-@app.callback(Output('output', 'children'), [Input('dummy-input', 'value')])
-def get_client_principal_name(dummy_value):
-    # Get the request context from the global Flask object
-    ctx = flask._app_ctx_stack.top
+dash_app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-    # Check if the request context exists
-    if ctx is not None:
-        # Access the request headers
-        request = flask.request
-        headers = request.headers
+    html.Div(children='''
+        This is Dash running on Azure App Service without a Docker container.
+    '''),
 
-        # Retrieve the client principal name from the headers
-        principal_name = headers.get('X-Ms-Client-Principal-Name')
-
-        if principal_name:
-            return f"Client Principal Name: {principal_name}"
-
-    # Return a default value if the client principal name is not found
-    return "Client Principal Name not found."
-
-# Define the layout of the app
-app.layout = dash.html.Div([
-    dash.dcc.Input(id='dummy-input', style={'display': 'none'}),
-    dash.html.Div(id='output')
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    dash_app.run_server(debug=True)
